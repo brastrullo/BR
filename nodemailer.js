0f1c5252563c8f8ecb71231c4b3ec66d71ffec51
brastrullo <brastrullo@gmail.com>
2015-10-15 11:32:22 -0400

    Update nodemailer.js

--- nodemailer.js
+++ nodemailer.js
@@ -9,14 +9,14 @@ var message = document.getElementById('message').value;
 var transporter = nodemailer.createTransport(smtpTransport({
   service: 'gmail',
   auth: {
-    user: 'brastrullo.dev@gmail.com',
-    pass: 'BRAR4S900'
+    user: '******@gmail.com',
+    pass: '********'
   }
 }));
 
 function sendMail(){
   transporter.sendMail({
-    from: name +' <brastrullo.dev@gmail.com>',
+    from: name +' <******@gmail.com>',
     to: 'bradleyrastrullo@gmail.com',
     replyTo: email,
     subject: 'Portfolio Site Contact Page',
